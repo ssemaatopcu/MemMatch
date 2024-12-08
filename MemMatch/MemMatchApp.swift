5348f7c90af32e0436c36b016f897df8202d09dd
Sema Topcu <ssemaatopcu.@gmail.com>
2024-12-08 18:02:38 +0300

    Initial Commit

---
+++ MemMatch/MemMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemMatchApp.swift
+//  MemMatch
+//
+//  Created by Sema Topcu on 12/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MemMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
